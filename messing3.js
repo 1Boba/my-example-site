14c5f986375aabb8ff2e7a6660871539a6b370dd
1Boba <bobasninja@gmail.com>
2022-10-24 01:08:12 -0700

    messing

---
+++ messing3.js
@@ -0,0 +1,53 @@
+async function start() {
+  const response = await fetch("https://dog.ceo/api/breeds/list/all")
+  const data = await response.json()
+  createBreedList(data.message)
+}
+
+start()
+
+function createBreedList(breedList) {
+  document.getElementById('breed').innerHTML = `
+  <select onchange="loadByBreed(this.value)">
+      <option>Choose a dog breed</option>
+      ${Object.keys(breedList).map(function (breed) {
+    return `<option>${breed}</option>`
+  }).join('')}
+    </select>
+  `
+}
+
+async function loadByBreed(breed) {
+  if (breed != "Choose a dog breed") {
+    const response = await fetch(`https://dog.ceo/api/breed/${breed}/images`)
+    const data = await response.json()
+    createSlideshow(data.message)
+  }
+}
+
+function createSlideshow(images) {
+  let currentPostion = 0
+  document.getElementById('slideshow').innerHTML = `
+    <div class="slide" style="background-image: url('${images[0]}')"></div>
+    <div class="slide" style="background-image: url('${images[1]}')"></div>
+    `
+  currentPostion += 2
+  setInterval(nextSlide, 3000)
+
+  function nextSlide() {
+    document.getElementById("slideshow").insertAdjacentHTML("beforeend", `
+        <div class="slide" style="background-image: url('${images[currentPostion]}')"><div>`)
+    setTimeout(aF, 1000)
+    if (currentPostion + 1 >= images.length) {
+      currentPostion = 0
+    } else {
+      currentPostion++
+    }
+  }
+  
+  function aF() {
+      document.querySelector(".slide").remove()
+    }
+}
+
+
